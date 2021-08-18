133d069a2d12da7669962c6159a192d5b10639e3
gohdong <aldehf420@naver.com>
2021-08-18 23:18:34 +0900

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+
+# "Host","Log Name","Date Time","Time Zone","Method","URL","Response Code","Bytes Sent","Referer","User Agent"
+log_list = []
+file_list = ['210725.csv','210802.csv','210808.csv','210815.csv','test.csv']
+for file in file_list:
+    with open(file, newline='') as csvfile:
+        readCSV = csv.reader(csvfile)
+        for row in readCSV:
+            log_list.append({
+                "host" : row[0],
+                "dateTime" : row[2],
+                "method" : row[4],
+                "url" : row[5],
+                "responseCode" : row[6],
+                "byteSent" : row[7],
+                "referer" :row[8] 
+            })
+
+
+# print(len(log_list))
+for log in log_list:
+    print(log)
+# print(log_list)
